58210d0f56949880c25f6270572e3d886da87a70
altaa <altaamgl@gmail.com>
2014-09-23 21:25:16 -0700

    Array fields

--- mesh/curve_gen2d.py
+++ mesh/curve_gen2d.py
@@ -5,13 +5,14 @@ import math
 from sets import Set
 
 import numpy as np
+import numpy.ma as ma
 from meshpy.triangle import MeshInfo, build, write_gnuplot_mesh
 
 import matplotlib.pyplot as plt
 
 class Mesh():
     shapes = ['rectangle', 'triangle', 'circle']
-    def __init__(self, points=[], facets=[], max_volume=50):
+    def __init__(self, points=[], facets=[], max_volume=100):
         self.points = points
         self.facets = facets
         self.max_volume = max_volume
@@ -34,9 +35,25 @@ class Mesh():
         self.mesh_info.set_points(self.points)
         self.mesh_info.set_facets(self.facets)
         self.mesh = build(self.mesh_info, generate_faces=True, max_volume=self.max_volume)
-        self.mesh_points = np.asarray(self.mesh.points)
-        self.mesh_faces = np.asarray(self.mesh.faces)
-        self.mesh_elements = np.asarray(self.mesh.elements)
+        print type(self.mesh.points)
+        self.mesh_points = np.array(np.array(self.mesh.points), dtype=[("x", float), ("y", float)]).reshape(len(self.mesh.points), 2)
+        self.mesh_point_indices = np.array(np.array(range(0, len(self.mesh.points))), dtype=[("idx", float)]).reshape(len(self.mesh.points), 1)
+        print type(self.mesh_point_indices), self.mesh_point_indices.shape
+        print type(self.mesh_points), self.mesh_points.shape
+
+        print self.mesh_points.x
+        self.mesh_points = np.concatenate((self.mesh_point_indices, self.mesh_points), axis=1)
+
+        self.mesh_faces = np.array(self.mesh.faces).reshape(len(self.mesh.faces), 2)
+        self.mesh_face_indices = np.array(range(0, len(self.mesh.faces))).reshape(len(self.mesh.faces), 1)
+        self.mesh_faces = np.hstack((self.mesh_face_indices, self.mesh_faces))
+
+        self.mesh_elements = np.array(self.mesh.elements).reshape(len(self.mesh.elements), 3)
+        self.mesh_element_indices = np.array(range(0, len(self.mesh.elements))).reshape(len(self.mesh.elements), 1)
+        self.mesh_elements = np.hstack((self.mesh_element_indices, self.mesh_elements))
+        self.mesh_points_ordered = np.sort(self.mesh_points, order="x")
+        print self.mesh_points_ordered
+
 
     def to_string(self):
         print "Mesh Points:"
@@ -65,11 +82,12 @@ class Mesh():
             print i, edge
     
     def generate_curve(self, func_str=None):
-        sample_points = random.sample(self.mesh.points, int(math.sqrt(len(self.mesh.points))))
+        sample_points = random.sample(self.mesh_points, int(math.sqrt(self.mesh_points.size)))
         sample_points = np.array(sample_points)
-        sample_Xs = np.sort(Set(sample_points[:,0]))
-        sample_points = np.concatenate((sample_Xs, self.find_closest_point(func_str, sample_Xs), axis=1)
+        print "Sample points"
         print sample_points
+        #sample_points = np.concatenate((sample_Xs, self.find_closest_point(func_str, sample_Xs)), axis=1)
+        self.find_closest_point(func_str, sample_points)
 
         func_points = []
         func_edges = []
@@ -78,12 +96,19 @@ class Mesh():
         Mesh.disp_edges(func_edges)
         self.plot()
         self.plot_curve(func_points, func_edges)
-    def find_closest_points(func_str, sample_Xs):
-        func_values = Mesh.vectorize_func(sample_Xs) 
-        for x in sample_Xs:
-            for y in self.mesh_points[mesh_points[:,0]==x][:,1]
 
-        
+    def find_closest_point(self, func_str, sample_points):
+        sample_Xs = np.sort(np.unique(sample_points[:,1]))
+        func_values = Mesh.vectorize_func(func_str, sample_Xs)[:,1] 
+        print sample_Xs
+        for i, x in enumerate(sample_Xs):
+            y = self.mesh_points[self.mesh_points[:,1]==x][:,2]
+            #print  abs(func_values[i] - y)
+        return []
+
+    def find_interval_points(self, x):
+        pass
+         
         
 
     def find_closest_edge(self, prev_edge_idx, curr_point, next_point=[]):
@@ -122,7 +147,7 @@ class Mesh():
         return x
 
     @staticmethod
-    def vectorize_func(func_str, x, x_range):
+    def vectorize_func(func_str, X):
         func_points = []
         if func_str.find(".") != -1:
             mod_name, func_name = func_str.rsplit('.', 1)
@@ -132,7 +157,6 @@ class Mesh():
         else:
             func = getattr(Mesh, func_str)
             vec_func = np.vectorize(func)    
-        X = np.array(range(x, x + x_range, 1))
         Y = vec_func(X)
         X = X.reshape(X.size, 1)
         Y = Y.reshape(Y.size, 1)
