e33e5ac68542e5534c13e9e3b8b2694af4488b53
altaa <altaamgl@gmail.com>
2015-02-09 11:01:31 -0800

    changes

--- mean_curve_demo.py
+++ mean_curve_demo.py
@@ -145,7 +145,7 @@ if __name__ == '__main__':
 #            k_currents = 2
                 for comb in combinations[:-1,:]:
                     input_currents = currents*comb.reshape(comb.size,1) 
-                    x, q, r, norm = mean.mean(mesh.points, mesh.simplices, mesh.edges, input_currents, l, opt, w, v, cons)
+                    t, q, r, norm = mean.mean(mesh.points, mesh.simplices, mesh.edges, input_currents, l, opt, w, v, cons)
                     norms.append(norm)
                     if norm < min_norm:
                         min_norm = norm
@@ -155,21 +155,7 @@ if __name__ == '__main__':
                         min_r = r
                         min_currents = input_currents
                     fig.clf()
-                    plt.gca().set_aspect('equal')
-                    plt.ylim([mesh.boundary_box[1]-5, mesh.boundary_box[3]+20])
-                    plt.xlim([mesh.boundary_box[0]-5, mesh.boundary_box[2]+5])
-                    rows = 2
-                    if opt == 'msfn' and len(functions) == 3:
-                        rows = 3
-                    elif len(functions) == 6:
-                        rows = 4
-                    #plt.subplot(rows, 2, 1)
-                    mesh.plot()
-                    for i, c in enumerate(input_currents):
-                        mesh.plot_curve(c, color=colors.next(), label='%s, %d'%(functions[i], comb[i]), linewidth=5)
-                    title = '%s, lambda=%.04f, %s' % (opt, l, str(comb))
-                    mesh.plot_curve(x, title)
-                    plt.legend(loc='upper right')
+                    plotting.plot_mean(mesh, input_currents, t)
                     figname = '/home/altaa/fig_dump/%d-%s-%.04f.png'%(figcount, opt, l)
                     plt.savefig(figname, dpi=fig.dpi)
                     pdf_file.savefig(fig)
@@ -183,32 +169,12 @@ if __name__ == '__main__':
 #                        mesh.plot()
 #                        mesh.plot_curve(c, color=colors.next(), linewidth=5, \
 #                        label='%s, %d'%(functions[i], comb[i]))
-#                        mesh.plot_curve(x, title, label='Mean')
+#                        mesh.plot_curve(t, title, label='Mean')
 #                        plt.legend(loc='upper right')
 #                        figname = '/home/altaa/fig_dump/%d-%s-%.04f.png'%(figcount, opt, l)
 #                        plt.savefig(figname, dpi=fig.dpi)
 #                        pdf_file.savefig(fig)
 #                        figcount += 1
-                    for i, r_i in enumerate(r):
-                        color = colors.next()
-                        fig.clf()
-                        plt.gca().set_aspect('equal')
-                        plt.ylim([mesh.boundary_box[1]-5, mesh.boundary_box[3]+20])
-                        plt.xlim([mesh.boundary_box[0]-5, mesh.boundary_box[2]+5])
-                        mesh.plot()
-                        mesh.plot_simplices(r_i, color=color)
-                        mesh.plot_curve(q[i], title=title + ', Q%d&R%d'%(i+1,i+1), color='m', marker='*', linewidth=6, label='Q%d'%(i+1))
-                        mesh.plot_curve(x, linewidth=4, label="Mean")
-                        if opt =='msfn' and i== r.shape[0]-1:
-                            pass
-                        else:
-                            mesh.plot_curve(input_currents[i], color='r', ls='--', \
-                            label='%s, %d'%(functions[i], comb[i]))
-                        plt.legend(loc='upper right')
-                        figname = '/home/altaa/fig_dump/%d-%s-%.04f.png'%(figcount,opt,l)
-                        plt.savefig(figname, dpi=fig.dpi)
-                        pdf_file.savefig(fig)
-                        figcount += 1
 
                     
                     #figname = '/home/altaa/fig_dump/%d-%s-%s-%.04f.png'%(figcount, '-'.join(functions),opt,l)
